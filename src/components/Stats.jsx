a2a72cb62660562853b6457fb0bd7c8d3e4553b9
mvciek <mpeciakk@gmail.com>
2023-01-21 18:17:34 +0100

    stats icons fix

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,4 +1,5 @@
 import WhiteContainer from "@/components/Container/WhiteContainer"
+import Image from "next/image"
 
 export default function Stats() {
   return (
@@ -8,12 +9,21 @@ export default function Stats() {
         <span>Finsweet Users</span>
       </div>
       <div className="icons">
-        <div className="icon">logoipsum</div>
-        <div className="icon">logoipsum</div>
-        <div className="icon">logoipsum</div>
-        <div className="icon">logoipsum</div>
-        <div className="icon">logoipsum</div>
-        <div className="icon">logoipsum</div>
+        <div className="icon">
+          <Image src="stats-logo1.svg" alt="stats-logo1" fill />
+        </div>
+        <div className="icon">
+          <Image src="stats-logo2.svg" alt="stats-logo2" fill />
+        </div>
+        <div className="icon">
+          <Image src="stats-logo3.svg" alt="stats-logo3" fill />
+        </div>
+        <div className="icon">
+          <Image src="stats-logo4.svg" alt="stats-logo4" fill />
+        </div>
+        <div className="icon">
+          <Image src="stats-logo5.svg" alt="stats-logo5" fill />
+        </div>
       </div>
     </div>
   )

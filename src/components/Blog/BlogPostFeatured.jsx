ae48bed5e66b6e9c0635c58e8589688639314478
mvciek <mpeciakk@gmail.com>
2023-01-22 01:02:44 +0100

    blog fix

--- src/components/Blog/BlogPostFeatured.jsx
+++ src/components/Blog/BlogPostFeatured.jsx
@@ -1,5 +1,6 @@
 import WhiteContainer from "@/components/Container/WhiteContainer"
 import BlogPostTitle from "@/components/Blog/BlogPostTitle"
+import Link from "next/link"
 
 export default function BlogPostFeatured() {
   return (
@@ -10,7 +11,9 @@ export default function BlogPostFeatured() {
         <div className="description">
           <p>Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside.</p>
 
-          <span>Read more</span>
+          <span>
+            <Link href="blog-read">Read more</Link>
+          </span>
         </div>
       </div>
     </WhiteContainer>
